c6c0cfead055a04d25347e633175dbd07e1337ce
Luis Alfonso Maeda Nunez <luis.maeda@tec.mx>
2021-03-22 20:00:33 -0600

    Started with first button input declaration in buttonled project

--- buttonled/buttonled.c
+++ buttonled/buttonled.c
@@ -17,20 +17,35 @@
 
 #include "MKL25Z4.h"
 
+//GREEN LED IS IN PORTB PIN19
 #define GREEN_LED_BIT 19
 #define GREEN_LED_PCR PORTB->PCR[GREEN_LED_BIT]
+#define RED_LED_BIT 18
+#define RED_LED_PCR PORTB->PCR[RED_LED_BIT]
+//Button0 is at PTA1
+#define BUTTON0_BIT 1
+#define BUTTON0_PCR PORTA->PCR[BUTTON0_BIT]
+
 
 void delayMs(int n);
 
 int main (void) {
   //Setup
-    SIM->SCGC5 |= 0x400;
+    //SIM->SCGC5 |= 0x400; //Enable PORTB clock gate
+    SIM->SCGC5 |= SIM_SCGC5_PORTB(1); //Enable PORTB clock gate
+    SIM->SCGC5 |= SIM_SCGC5_PORTA(1); //Enable PORTA clock gate
     
-    GREEN_LED_PCR = 0x0100;
-    GPIOB->PDDR |= 0x80000;          /* make PTB19 as output pin */
+    //PORTB->PCR[19] = 0x0100;
+    GREEN_LED_PCR = PORT_PCR_MUX(1); // Set GREEN_LED bit as GPIO
+    RED_LED_PCR = PORT_PCR_MUX(1); // Set RED_LED bit as GPIO
+    BUTTON0_PCR = PORT_PCR_MUX(1) | PORT_PCR_PE(1) | PORT_PCR_PS(1); // Set port pin as GPIO with pull up resistor enabled
+    //GPIOB->PDDR |= 0x80000;          /* make PTB19 as output pin */
+    GPIOB->PDDR |= (1<<GREEN_LED_BIT); //Set GREEN_LED_BIT pin as output
+    GPIOB->PDDR |= (1<<RED_LED_BIT); //Set RED_LED_BIT pin as output
 
     while (1) {
       //Loop
+        GPIOB->PDOR &= ~(1<<RED_LED_BIT);
         GPIOB->PDOR &= ~0x80000;     /* turn on green LED */
         delayMs(250);
         GPIOB->PDOR |= 0x80000;      /* turn off green LED */
