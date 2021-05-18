761ead6cf50af2df76dcac408ea7b333ebded984
Luis Alfonso Maeda Nunez <luis.maeda@tec.mx>
2021-05-17 19:26:37 -0500

    SysTick demo code

--- systick_demo/systick_demo.c
+++ systick_demo/systick_demo.c
@@ -1,28 +1,60 @@
 /*
  * SysTick Demo
+ * ARM Cortex M0 SysTick Timer
  */
 
 #include "MKL25Z4.h"
-#include "system_MKL25Z4.h"
 
+#define GREEN_LED_BIT 19
+#define GREEN_LED_PCR PORTB->PCR[GREEN_LED_BIT]
+#define GREEN_LED_PORT_OUT GPIOB->PDOR
 
 void delayMs(int n);
+void delay1Ms(void);
 
 int main(void)
 {
-  SystemCoreClockUpdate(); //Setting clock to 20.97MHz
+  SIM->SCGC5 |= SIM_SCGC5_PORTB(1); //Enable PORTB clock gate
+  GREEN_LED_PCR = PORT_PCR_MUX(1); // Set GREEN_LED bit as GPIO
+  GPIOB->PDDR |= (1<<GREEN_LED_BIT); //Set GREEN_LED_BIT pin as output
+
   for(;;)
   {
+    GREEN_LED_PORT_OUT ^= (1<<GREEN_LED_BIT);
+    delayMs(1000);
   }
 }
 
 
-/* Delay n milliseconds
- * The CPU core clock is set to MCGFLLCLK at 41.94 MHz in SystemInit().
+/* ARM SysTick Delay n milliseconds
+ * The CPU core clock is set to MCGFLLCLK at 20.97 MHz in SystemInit().
  */
 void delayMs(int n) {
-    int i;
-    int j;
-    for(i = 0 ; i < n; i++)
-        for(j = 0 ; j < 7000; j++) { }
+  SysTick->CTRL = 0; //Disable SysTick
+  SysTick->LOAD = 20970;
+  SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk; //0x05
+  for(int i=0; i<n; i++){
+    while((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) != SysTick_CTRL_COUNTFLAG_Msk);
+  }
+  SysTick->CTRL = 0; //Disable SysTick
 }
+
+
+void delay1Ms(void) {
+  SysTick->CTRL = 0; //Disable SysTick
+  SysTick->LOAD = 20970;
+  SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk; //0x05
+  while((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) != SysTick_CTRL_COUNTFLAG_Msk);
+  SysTick->CTRL = 0; //Disable SysTick
+}
+
+
+
+
+
+
+
+
+
+
+
