146e7eaafe27599e55253be9dc207588ee6bd908
Luis Alfonso Maeda Nunez <luis.maeda@tec.mx>
2021-05-20 19:05:06 -0500

    TPM demo with prescaler clk over 128

--- timer_demo/tpm_demo.c
+++ timer_demo/tpm_demo.c
@@ -1,9 +1,17 @@
-/* p5_4.c Toggling blue LED using TPM0 delay
+/* TPM Demo:
+ * Toggling blue LED using TPM0 delay
+ * MCGFLLCLK = 20.97MHz
  */
 
-#include <MKL25Z4.H>
-#include "system_MKL25Z4.h"
+#include "MKL25Z4.h"
 
+//163.840kHz
+//32768
+//5
+
+//20.97MHz
+//65536
+//320
 
 int main(){
   int i=0;
@@ -15,11 +23,11 @@ int main(){
   SIM->SCGC6 |= SIM_SCGC6_TPM0_MASK; //Enable TPM0 Clock gate
   SIM->SOPT2 = SIM_SOPT2_TPMSRC(1);  //MCGFLLCLK
   TPM0->SC = 0;                      //Disable timer
-  TPM0->MOD = 32767;                //Set Modulo reg to 65535
-  TPM0->SC |= TPM_SC_TOF_MASK;       //Clear TOF flag
+  TPM0->MOD = 32768-1;                //Set Modulo reg to 32768
+  TPM0->SC |= TPM_SC_TOF_MASK | TPM_SC_PS(7);       //Clear TOF flag and Prescaler to clk/128
   TPM0->SC |= TPM_SC_CMOD(1);        //Set TPM input clock and enable
     while (1) {
-        for(i = 0; i < 320; i++) {  /* repeat timeout for 320 times */
+        for(i = 0; i < 5; i++) {  /* repeat timeout for 5 times */
             while((TPM0->SC & TPM_SC_TOF_MASK) == 0) { }/* wait until the TOF is set */
             TPM0->SC |= TPM_SC_TOF_MASK;   /* clear TOF */
         }
